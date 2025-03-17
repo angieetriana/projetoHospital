2373f69bac1ecf6525a3b82771759159f434065e
Angie Triana <92684096+angieetriana@users.noreply.github.com>
2025-03-17 17:42:39 -0300

    Adicionei menu e funcao para cadastro dos pacientes

--- cadastropacientes.py
+++ cadastropacientes.py
@@ -1,3 +1,14 @@
+lista_pacientes = []
+
+def mostrar_menu_principal():
+    print("----- MENU PRINCIPAL -----")
+    print("(1) Adicionar paciente")
+    print("(2) Mostrar paciente")
+    print("(3) Reavaliar")
+    print("(4) Excluir paciente")
+    print("(0) Sair")
+    return input("Digite uma opção válida: ")
+
 def altura_estimada_mulher(aj, idade):
    return ((1.83 * aj) + (0.24 * idade) + 84.88)/100
 
@@ -10,26 +21,37 @@ def peso_estimado_homem(cb, aj):
 def peso_estimado_mulher(cb, aj):
    return aj * 1.09 + cb * 2.68 - 65.51
 
-idade = int(input('Digite a idade do paciente: '))
-sexo = input('Qual o sexo do paciente?(M/F) ').strip().upper()
+def cadastrar_paciente():
+   nome = input('Digite o nome: ')
+   idade = int(input('Digite a idade do paciente: '))
+   sexo = input('Qual o sexo do paciente?(M/F) ').strip().upper()
 
-cb = float(input('Digite CB: '))
-cp = float(input('Digite CP: '))
-aj = float(input('Digite AJ: '))
+   cb = float(input('Digite CB: '))
+   cp = float(input('Digite CP: '))
+   aj = float(input('Digite AJ: '))
 
-if sexo == 'F':
-    altura_estimada = altura_estimada_mulher(aj, idade)
-    peso_estimado = peso_estimado_mulher(cb, aj) 
-elif sexo == 'M':
-   altura_estimada = altura_estimada_homem(aj, idade)
-   peso_estimado = peso_estimado_homem(cb, aj) 
-else: 
-   print('Valor incorreto.')
-   altura_estimada = None
-   peso_estimado = None
+   if sexo == 'F':
+      altura_estimada = altura_estimada_mulher(aj, idade)
+      peso_estimado = peso_estimado_mulher(cb, aj) 
+   elif sexo == 'M':
+      altura_estimada = altura_estimada_homem(aj, idade)
+      peso_estimado = peso_estimado_homem(cb, aj) 
+   else: 
+      print('Valor incorreto.')
+      altura_estimada = None
+      peso_estimado = None
 
-imc = peso_estimado / (altura_estimada * altura_estimada)
+   imc = peso_estimado / (altura_estimada * altura_estimada)
 
-print(altura_estimada)
-print(peso_estimado)
-print(imc)
+   cadastro_paciente = {
+      "nome": nome,
+      "Idade": idade,
+      "Sexo": sexo,
+      "C.B": cb,
+      "C.P": cp,
+      "A.J": aj,
+      "Peso estimado": peso_estimado,
+      "Altura estimada": altura_estimada,
+      "IMC": imc      
+   }
+   lista_pacientes.append(cadastro_paciente)
